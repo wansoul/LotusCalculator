d9a384f4b52ac91fa290b59b8a373c85f4e7c278
Wan Souza <contato@wansouza.com>
2017-04-02 11:04:42 -0300

    Update service-worker link
    Update service-worker link

--- js/app.js
+++ js/app.js
@@ -54,12 +54,3 @@ app.controller("calculatorCtrl", function($scope){
   }
   
 });
-
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-    console.log('Service Worker registered');
-  }).catch(function(err) {
-    console.log('Service Worker registration failed: ', err);
-  });
-}
