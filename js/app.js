0f288a50ccd87b8e00b0edbe5fecd1825055bf45
Wan Souza <contato@wansouza.com>
2017-04-03 08:45:11 -0300

    Changing analytics script location
    Changing analytics script location

--- js/app.js
+++ js/app.js
@@ -63,3 +63,19 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker registration failed: ', err);
   });
 }
+
+
+
+if (window.matchMedia('(display-mode: standalone)').matches) {
+  console.log("Thank you for installing our app!");
+} else {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-96596472-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+}
